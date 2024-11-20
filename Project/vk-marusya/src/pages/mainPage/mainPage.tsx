f7f83b662b0bad9fd7a87906059cd3ddb59c369a
Vega7 <anmax3@mail.ru>
2024-11-20 06:34:15 +0300

    in progress

--- Project/vk-marusya/src/pages/mainPage/mainPage.tsx
+++ Project/vk-marusya/src/pages/mainPage/mainPage.tsx
@@ -5,18 +5,24 @@ import { ListTop } from '../../components/ListTop/ListTop'
 import { getMovies, getRandomMovie } from '../../api/Movies'
 import { getGenresList } from '../../utils/genreList'
 import { convertRunTime } from '../../utils/convertRunTime'
+import { ratingColor } from '../../utils/ratingColor'
 
 const items = await getMovies();
 const randomMovie = await getRandomMovie();
 
 export const MainPage = () => {
+
+    const handleRerender = () => {
+        getRandomMovie();
+    }
+
     return (
         <div className={styles.main__container}>
             <div className={styles.random__container}>
                 <div className={styles.left__content}>
                     <div className={styles.random__stats}>
                         <div className={styles.stats__item}>
-                            <div className={styles.random__rating}>
+                            <div className={styles.random__rating} style={{ backgroundColor: ratingColor(randomMovie.tmdbRating) }}>
                                 <span className={styles.rating__star}>
                                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M8.00105 12.1734L3.29875 14.8055L4.34897 9.51997L0.392578 5.86124L5.74394 5.22675L8.00105 0.333374L10.2581 5.22675L15.6095 5.86124L11.6531 9.51997L12.7033 14.8055L8.00105 12.1734Z" fill="white" />
@@ -50,7 +56,7 @@ export const MainPage = () => {
                             </svg>
 
                         </Button>
-                        <Button className={styles.random__button} onClick={() => { }}>
+                        <Button className={styles.random__button} onClick={handleRerender}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 4C14.7486 4 17.1749 5.38626 18.6156 7.5H16V9.5H22V3.5H20V5.99936C18.1762 3.57166 15.2724 2 12 2C6.47715 2 2 6.47715 2 12H4C4 7.58172 7.58172 4 12 4ZM20 12C20 16.4183 16.4183 20 12 20C9.25144 20 6.82508 18.6137 5.38443 16.5H8V14.5H2V20.5H4V18.0006C5.82381 20.4283 8.72764 22 12 22C17.5228 22 22 17.5228 22 12H20Z" fill="white" />
                             </svg>
