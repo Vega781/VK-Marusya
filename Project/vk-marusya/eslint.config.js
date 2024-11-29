9f63dc638d714af8e4c4b2b7bc0a96c0c4a85b83
Vega7 <anmax3@mail.ru>
2024-11-29 13:21:17 +0300

    update files

--- Project/vk-marusya/eslint.config.js
+++ Project/vk-marusya/eslint.config.js
@@ -1,28 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.d.ts'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+      }
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      'no-undef': 'off',
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react': react
+    },
+    rules: {
+      'no-undef': 'off',
+      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
-  },
-)
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+]
